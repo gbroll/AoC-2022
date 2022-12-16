46a1e2240a1752100cb067f24f9c2b4a3f584362
gbroll <gustav.brolin@gmail.com>
2022-12-16 21:28:23 +0100

    Add some type defs

--- common.h
+++ common.h
@@ -1,8 +1,17 @@
 
 #ifndef COMMON_H
 #define COMMON_H
-enum part {
+
+#define TRUE 1
+#define FALSE 0
+
+typedef __uint8_t u8;
+typedef __uint16_t u16;
+typedef __uint32_t u32;
+
+typedef enum part {
     PART_ONE = 0,
     PART_TWO = 1
-};
+} Part;
+
 #endif /* COMMON_H */
